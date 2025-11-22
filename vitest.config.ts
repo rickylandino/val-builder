3ae0c20c7a1175de71f201446554f4100e9b44dd
Ricky Landino <ricky@landino.net>
2025-11-22 07:13:35 -0500

    more property updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,9 @@ export default defineConfig({
         '**/*.config.{ts,js}',
         'src/main.tsx',
         'src/App.tsx',
+        '**/test/**',
+        '**/config/**',
+        'coverage/'
       ],
     },
   },
